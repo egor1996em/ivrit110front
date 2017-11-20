33dc2f12a1dc733327645a15350c1c4e542d5dbf
egor1996em <egor1996em@gmail.com>
2017-11-21 00:24:53 +0300

    Add files via upload

--- js/bookCoverLoader.js
+++ js/bookCoverLoader.js
@@ -1,23 +1,43 @@
 $(document).ready(function (e) {
     setBookPicture();
+
+    $(window).resize(function (){
+        setBookPicture();
+    });
 });
 
 //id контейнера - назвние картинки учебника и его идентификация на странице
 function setBookPicture(){
         var book = $('.book-page-container');
         var windowWidth = $(window).width();
-        var directoryName;
+        var directoryName = getDirectoryName(windowWidth);
         var URL;
-        if (windowWidth > 1199){
-        directoryName = "large";
-        }
+
         URL = createURLtoBookImage(directoryName, $(book).attr("id"));
         $(book).children('.book-cover').attr('style', 'background: url("' + URL + '") no-repeat; ' +
         'background-size: contain;');
 }
 
 function createURLtoBookImage(directoryName, filename) {
-    var URL = 'images/books/' + directoryName + '/' + filename + '.jpg';
+    var URL = 'images/books/' + directoryName + '/' + filename + '.png';
 
     return URL;
+}
+
+function getDirectoryName(windowWidth){
+    var directoryName;
+    if (windowWidth > 1199){
+        directoryName = "large";
+    } else if (windowWidth <= 1199 && windowWidth > 992) {
+        directoryName = "big";
+    }
+    else if (windowWidth <= 992 && windowWidth > 768) {
+        directoryName = "middle";
+    } else if (windowWidth <= 768 && windowWidth > 544) {
+        directoryName = "small";
+    } else {
+        directoryName = "extrasmall";
+    }
+
+    return directoryName;
 }
--- js/categorybookslist.js
+++ js/categorybookslist.js
@@ -1,5 +1,9 @@
 $(document).ready(function (e) {
     setPicturesToBooks();
+
+    $(window).resize(function (){
+        setPicturesToBooks();
+    });
 });
 
 //id контейнера - назвние картинки учебника и его идентификация на странице
@@ -9,11 +13,8 @@ function setPicturesToBooks(){
     for (var i = 0; i < booksOnPage.length; i++){
         var book = $(booksOnPage[i]);
         var windowWidth = $(window).width();
-        var directoryName;
+        var directoryName = getDirectoryName(windowWidth);
         var URL;
-        if (windowWidth > 1199){
-            directoryName = "large";
-        }
 
         URL = createURLtoBookImage(directoryName, $(book).attr("id"));
         $(book).children('.book-cover').attr('style', 'background: url("' + URL + '") no-repeat; ' +
@@ -23,7 +24,25 @@ function setPicturesToBooks(){
 }
 
 function createURLtoBookImage(directoryName, filename) {
-    var URL = 'images/books/' + directoryName + '/' + filename + '.jpg';
+    var URL = 'images/books/' + directoryName + '/' + filename + '.png';
 
     return URL;
+}
+
+function getDirectoryName(windowWidth){
+    var directoryName;
+    if (windowWidth > 1199){
+        directoryName = "large";
+    } else if (windowWidth <= 1199 && windowWidth > 992) {
+        directoryName = "big";
+    }
+    else if (windowWidth <= 992 && windowWidth > 768) {
+        directoryName = "middle";
+    } else if (windowWidth <= 768 && windowWidth > 544) {
+        directoryName = "small";
+    } else {
+        directoryName = "extrasmall";
+    }
+
+    return directoryName;
 }
